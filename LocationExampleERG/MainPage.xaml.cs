5702b5dec1ea599195fc37f811beb91d10eb2aac
EGartin <elijah.gartin@gmail.com>
2013-04-09 22:31:05 -0600

    Added GPS Functionality to pinpoint your location

--- LocationExampleERG/MainPage.xaml.cs
+++ LocationExampleERG/MainPage.xaml.cs
@@ -10,15 +10,45 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using System.Device.Location;
 
 namespace LocationExampleERG
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        GeoCoordinateWatcher gcw;
+
         // Constructor
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            gcw = new GeoCoordinateWatcher(GeoPositionAccuracy.High);
+            gcw.MovementThreshold = 20;
+            gcw.StatusChanged += new EventHandler<GeoPositionStatusChangedEventArgs>(gcw_StatusChanged);
+            gcw.Start();
+        }
+
+        private void gcw_StatusChanged(object sender, GeoPositionStatusChangedEventArgs e)
+        {
+            //throw new NotImplementedException();
+            if (e.Status == GeoPositionStatus.Ready)
+            {
+                GeoCoordinate coord = gcw.Position.Location;
+                latitude.Text = "Latitude: " + coord.Latitude.ToString("0.000");
+                longitude.Text = "Longitude: " + coord.Longitude.ToString("0.000");
+                //After you have the location, stop the service to conserve power
+                gcw.Stop();
+            }
+            if (e.Status == GeoPositionStatus.Disabled || e.Status == GeoPositionStatus.NoData)
+            {
+                latitude.Text = "GPS Disabled";
+                longitude.Text = "";
+                gcw.Stop();
+            }
         }
     }
 }
